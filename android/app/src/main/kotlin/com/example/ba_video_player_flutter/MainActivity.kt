88eed7a767d803f1b26cd106505948b7846773ed
Behzad Ayubifar <behzad13811374@gmail.com>
2023-10-26 23:08:12 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/ba_video_player_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ba_video_player_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/video_player_page.dart
@@ -0,0 +1,115 @@
+import 'package:appinio_video_player/appinio_video_player.dart';
+import 'package:flutter/material.dart';
+
+enum Source { network, asset }
+
+class VideoPlayePage extends StatefulWidget {
+  const VideoPlayePage({super.key});
+
+  @override
+  State<VideoPlayePage> createState() => _VideoPlayePageState();
+}
+
+class _VideoPlayePageState extends State<VideoPlayePage> {
+  late CustomVideoPlayerController _customVideoPlayerController;
+
+  Source currentSource = Source.asset;
+
+  String assetVideopath = "assets/videos/le.mp4";
+
+  late bool isLoading;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeVideoPlayer(currentSource);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _customVideoPlayerController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                CustomVideoPlayer(
+                  customVideoPlayerController: _customVideoPlayerController,
+                ),
+                _sourceButtons(),
+              ],
+            ),
+    );
+  }
+
+  void initializeVideoPlayer(Source source) {
+    isLoading = true;
+    VideoPlayerController videoPlayerController;
+    if (source == Source.asset) {
+      videoPlayerController = VideoPlayerController.asset(assetVideopath)
+        ..initialize().then((_) {
+          setState(() {
+            isLoading = false;
+          });
+        });
+    } else if (source == Source.network) {
+      videoPlayerController = VideoPlayerController.networkUrl(
+        Uri.parse(
+            "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4"),
+      )..initialize().then((_) {
+          setState(() {
+            isLoading = false;
+          });
+        });
+    } else {
+      return;
+    }
+
+    _customVideoPlayerController = CustomVideoPlayerController(
+      context: context,
+      videoPlayerController: videoPlayerController,
+    );
+  }
+
+  Widget _sourceButtons() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        MaterialButton(
+            color: Colors.red,
+            child: const Text(
+              "Network",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              setState(() {
+                currentSource = Source.network;
+                initializeVideoPlayer(currentSource);
+              });
+            }),
+        MaterialButton(
+            color: Colors.red,
+            child: const Text(
+              "Asset",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              setState(() {
+                currentSource = Source.asset;
+                initializeVideoPlayer(currentSource);
+              });
+            }),
+      ],
+    );
+  }
+}
